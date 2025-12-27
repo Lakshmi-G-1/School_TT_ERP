d40383da558b9aa02b9213d41491d8cfdf66fc34
Lakshmi-G-1 <lakshmimatajps@gmail.com>
2025-12-27 08:31:07 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('view/', views.view_timetable, name='view_timetable'),
-    path('generate/', views.generate_timetable_view, name='generate'),
-]
