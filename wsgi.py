a96b00e5984866aa84bcf32fa5e9c738c1ff6127
Lakshmi-G-1 <lakshmimatajps@gmail.com>
2025-12-27 08:31:27 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# This must also point to 'core.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-application = get_wsgi_application()
